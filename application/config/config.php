4186e61e4918e9d779710b18a225bc4761957b79
Syahrul Azlanuddin Remeli <malaysiawap@gmail.com>
2017-08-05 06:48:52 +0800

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,8 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-//$config['base_url'] = 'http://localhost/soding_todo/';
-$config['base_url'] = 'https://lan89.github.io/soding-todo/';
+$config['base_url'] = 'http://localhost/soding_todo/';
 
 /*
 |--------------------------------------------------------------------------
